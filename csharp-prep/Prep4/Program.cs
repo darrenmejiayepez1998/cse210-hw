6e172c76f34652c77c7ba0e0185fbb1f13f64714
darren.mejia.yepez1998 <darren.mejia.yepez@gmail.com>
2023-10-30 00:40:08 -0600

    completed the prep programs

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,54 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+            
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        // compute the sum
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        
+        Console.WriteLine($"The sum is: {sum}");
+
+        //Compute the average
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average number is: {average}");
+
+        //Find the max number
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+                // if number is greater than max then that is the new max!!
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
