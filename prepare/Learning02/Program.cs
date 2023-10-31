2facf3cb925f863e64c3e8c326a874358b009576
darren.mejia.yepez1998 <darren.mejia.yepez@gmail.com>
2023-10-31 02:40:23 -0600

    PREP ACTIVITY WEEK 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Loss mitigation agent";
+        job1._company = "Select Portfolio Services";
+        job1._startYear = 2021;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Sales Rep";
+        job2._company = "Moxie Pest Control";
+        job2._startYear = 2021;
+        job2._endYear = 2025;
+
+        Resume myResume = new Resume();
+        myResume._name = "Darren Mejia";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
