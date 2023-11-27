e7ba5add7a955134d02b36db5a06af7d6fd89589
darren.mejia.yepez1998 <darren.mejia.yepez@gmail.com>
2023-11-26 22:14:27 -0700

    made preparation prog w3

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,40 @@
+using System;
+
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        // Default to 1/1
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    // This is not a member variable
+    // It will be recomputed each time this is called
+    {
+        return (double)_top / (double)_bottom;
+    }
+}
